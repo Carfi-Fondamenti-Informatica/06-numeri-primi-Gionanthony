7b9693c2f4a085e4da81955b163de2b28e6aa527
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-03 10:49:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    int num;
+    cin >> num;
+    if(verifica(num) == true){
+        cout << "numero primo";
+    }
+    else{
+        cout << "numero non primo";
+    }
+    return 0;
 }
