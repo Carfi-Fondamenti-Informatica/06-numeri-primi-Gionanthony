5b89bcdfd3fe0a373ac2cbda429e2ed3b0bff888
Gionanthony <117936434+Gionanthony@users.noreply.github.com>
2022-12-03 10:48:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool verifica (int num) {
+    int c=0;
+
+    for (int i=1; i<=num; i++) {
+        if (num%i == 0) {
+            c++;
+        }
+    }
+
+    if (c==2) {
+        return true;
+    } else {
+        return false;
+    }
+}
